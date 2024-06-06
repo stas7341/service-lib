1bbe111c58789b0641973ef8ebbce2e7a27a3ad2
stasmataev <stas@ravin-ai.com>
2024-06-06 10:32:29 +0300

    fix output dir

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export * from "./services/logger";
+export * from "./services/configManager";
+export * from "./services/httpService";
+export * from "./services/mysqlService";
+export * from "./model/message";
+export * from "./utils/general";
+export * from "./utils/fileUtils";
+export * from "./services/amqpService";
+export * from "./utils/general";
+export * from "./utils/fileUtils";
+export * from "./interfaces/redis.interface";
+export * from "./utils/localCache";
+export * from "./utils/remoteCache";
+export * from "./model/types";
+export * from "./services/redisService";
+
+
+
+
+
